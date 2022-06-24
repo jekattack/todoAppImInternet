2d46c614fa5ca7e4a967048295a4733a1d0e0f43
jekattack <jan.erik.kraus@t-online.de>
2022-06-24 13:18:36 +0200

    Send a prayer 4

--- frontend/src/service/ApiService.ts
+++ frontend/src/service/ApiService.ts
@@ -3,32 +3,32 @@ import axios, {AxiosResponse} from "axios";
 
 export default function getRequest(){
     return (
-        axios.get('http://localhost:8080/api/kanban')
+        axios.get('/api/kanban')
             .then((response: AxiosResponse<Todo[], any>) => response.data)
     )
 }
 
 export function getForIdRequest(id: string){
     return (
-        axios.get(`http://localhost:8080/api/kanban/${id}`)
+        axios.get(`/api/kanban/${id}`)
             .then((response: AxiosResponse<Todo, any>) => response.data)
     )
 }
 
 export function postRequest(input: Todo){
     return(
-        axios.post(`http://localhost:8080/api/kanban/`, input)
+        axios.post(`/api/kanban/`, input)
     )
 }
 
 export function putRequest(urlInput: String, input: Todo){
     return(
-        axios.put(`http://localhost:8080/api/kanban/${urlInput}`, input)
+        axios.put(`/api/kanban/${urlInput}`, input)
     )
 }
 
 export function deleteRequest(urlInput: String){
     return(
-        axios.delete(`http://localhost:8080/api/kanban/${urlInput}`)
+        axios.delete(`/api/kanban/${urlInput}`)
     )
 }
