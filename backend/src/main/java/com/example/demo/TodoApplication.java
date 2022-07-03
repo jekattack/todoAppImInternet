efef2a3d17dc52af54edf1d549b08c20c67a62f5
jekattack <jan.erik.kraus@t-online.de>
2022-07-03 11:18:12 +0200

    RestTemplate Bean hinzugefügt

--- backend/src/main/java/com/example/demo/TodoApplication.java
+++ backend/src/main/java/com/example/demo/TodoApplication.java
@@ -2,13 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
